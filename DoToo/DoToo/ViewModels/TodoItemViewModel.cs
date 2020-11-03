1fccf3904cf4c936cbbd20f2e768c1e5d468f3f0
bquinn83 <45705195+bquinn83@users.noreply.github.com>
2020-11-03 12:55:38 -0700

    init commit

---
+++ DoToo/DoToo/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+using DoToo.Models;
+
+namespace DoToo.ViewModels
+{
+    public class TodoItemViewModel : ViewModel
+    {
+        public TodoItemViewModel(TodoItem item) => Item = item;
+
+        public event EventHandler ItemStatusChanged;
+        public TodoItem Item { get; private set; }
+        public string StatusText => Item.Completed ? "Reactivate" : "Completed";
+    }
+}
