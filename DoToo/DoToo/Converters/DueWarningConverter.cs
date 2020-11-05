59fa1ea0189da79105df73de8c7c21cbabe4e8f6
bquinn83 <45705195+bquinn83@users.noreply.github.com>
2020-11-05 13:40:28 -0700

    Due Date Color Converter
    Add 3 colors to show Due Date urgency

---
+++ DoToo/DoToo/Converters/DueWarningConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DoToo.Converters
+{
+    class DueWarningConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var daysUntilDue = ((DateTime)value - DateTime.Now).Days;
+            if (daysUntilDue <= 2)
+            {
+                return (Color)Application.Current.Resources["DangerColor"];
+            } else if (daysUntilDue <= 7)
+            {
+                return (Color)Application.Current.Resources["WarningColor"];
+            } else
+            {
+                return (Color)Application.Current.Resources["NormalColor"];
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
